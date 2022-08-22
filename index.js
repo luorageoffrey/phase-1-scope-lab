52f9335cf84b03815d0d4ee5d151602fdde26d8e
luorageoffrey <geoffrey.luora@student.moringaschool.com>
2022-08-22 09:29:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+customerName = "bob"
+
+function upperCaseCustomerName(){
+   customerName =  customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "somebody"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "nobody"
+}
